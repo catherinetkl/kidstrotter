10c326db4f43bdd6e6efc2204419f67abdea45c1
Catherine <70590533+catherinetkl@users.noreply.github.com>
2022-11-20 17:22:12 +0800

    added validations for activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ApplicationRecord
   has_many :bookmarks
   has_many :users, through: :bookmarks
+
+  validates :name, :location, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  validates :age_group, inclusion: { in: ['0-2', '3-5', '6-9', '10-12', '13-17'] }
 end
