92a67459178a1bd7b3704c3e3e30c1f9f3d468ed
Catherine <70590533+catherinetkl@users.noreply.github.com>
2022-11-20 16:52:34 +0800

    added routes for index and show activites

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "activities#index"
+
+  get '/activities', to: 'activities#index', as: :activities
+  get '/activities/:id', to: 'activities#show', as: :activity
 end
