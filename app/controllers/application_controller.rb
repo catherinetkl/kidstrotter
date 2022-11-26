e53b71e53577eb4d8b19c79cdbacae423ca16eb4
Catherine <70590533+catherinetkl@users.noreply.github.com>
2022-11-26 13:53:34 +0800

    include metatagshelper and default_url_options in applications controller, created meta tags helper, comment out private in activity model, added meta title, description and open graph to head of application.html.erb, created default meta in initializers and meta yml file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include MetaTagsHelper
+
+  def default_url_options
+    { host: ENV['DOMAIN'] || 'localhost:3000' }
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
