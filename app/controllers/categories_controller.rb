014ff9f2576a65b5b2a34f9822eb5b6bc1fff960
mirza shamsudin <mirza_shamsudin@hotmail.com>
2022-12-01 09:44:00 +0800

    Small debug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,18 @@
 class CategoriesController < ApplicationController
+  # before_action :set_category, only: %i[show]
+
   def index
     @categories = Category.all
     @activities = Activity.all
   end
 
   def show
+    # @activities = @category.activities
+
+    # # check if user searched for anything
+    # @activities = @activities.search_by_activity_and_category(params[:query]) if params[:query].present?
+    # # check if user filtered by price
+    # @activities = @activities.where(require_payment: params[:require_payment]) if params[:require_payment].present? && params[:require_payment] != 'all'
     @category = Category.find(params[:id])
     @activities = @category.activities
     # @map_activities = Activity.all
@@ -15,5 +23,14 @@ class CategoriesController < ApplicationController
         lng: event.longitude
       }
     end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+    @map_activities = Activity.all
+    @activities = @category.activities
+    # @map_activities = Activity.all
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -51,6 +51,4 @@ export default class extends Controller {
     }
 
 
-})
-
 }
